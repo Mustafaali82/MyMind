310c7a64fa141fce76582ce8f32348d0d51457d6
Mustafa Ali <mustafaali8254@gmail.com>
2024-02-08 00:16:43 +0500

    File Updated

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openBox(evet, boxName) {
+  var i, boxtabcontent, boxtablinks;
+  boxtabcontent = document.getElementsByClassName("boxtabcontent");
+  for (i = 0; i < boxtabcontent.length; i++) {
+    boxtabcontent[i].style.display = "none";
+  }
+  boxtablinks = document.getElementsByClassName("boxtablinks");
+  for (i = 0; i < boxtablinks.length; i++) {
+    boxtablinks[i].className = boxtablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(boxName).style.display = "block";
+  evet.currentTarget.className += " active";
+}
